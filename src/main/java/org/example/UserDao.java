3116b7919f7c13b70d8539aa6eee94cb7ebe1c63
Bagsangbin <Bagsangbin01@gmail.com>
2024-11-10 16:40:53 +0900

    클래스의 분리

--- src/main/java/org/example/UserDao.java
+++ src/main/java/org/example/UserDao.java
@@ -2,10 +2,15 @@ package org.example;
 
 import java.sql.*;
 
-public abstract class UserDao {
+public class UserDao {
+    private SimpleConnectionMaker simpleConnectionMaker;
+
+    public UserDao(){
+        simpleConnectionMaker = new SimpleConnectionMaker();
+    }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection c = getConnection();
+        Connection c = simpleConnectionMaker.makeNewConnection();
         PreparedStatement ps = c.prepareStatement("insert into users(id,name,password" +
                 ") values(?,?,?)");
         ps.setString(1,user.getId());
@@ -20,8 +25,7 @@ public abstract class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = getConnection();
+        Connection c = simpleConnectionMaker.makeNewConnection();
 
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
         ps.setString(1,id);
@@ -41,12 +45,12 @@ public abstract class UserDao {
     }
 
     //중복의 제거를 위해 Connection을 가져오는 부분을 따로 제작한다.
-    private abstract Connection getConnection() throws ClassNotFoundException, SQLException;
-    /*{
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/toby_example_db","root","root");
-        return c;
-    }*/
+//    private abstract Connection getConnection() throws ClassNotFoundException, SQLException;
+//    {
+//        Class.forName("com.mysql.cj.jdbc.Driver");
+//        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/toby_example_db","root","root");
+//        return c;
+//    }
 }
 
 public class NUserDao extends UserDao{
@@ -57,12 +61,12 @@ public class NUserDao extends UserDao{
         return c;
     }   //N사 생성 코드
 }
-
-public class DUserDao extends UserDao{
-    public Connection getConnection() throws ClassNotFoundException, SQLException
-    { 
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/toby_example_db","root","root");
-        return c;
-    }   //D사 생성 코드
-}
+//
+//public class DUserDao extends UserDao{
+//    public Connection getConnection() throws ClassNotFoundException, SQLException
+//    {
+//        Class.forName("com.mysql.cj.jdbc.Driver");
+//        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/toby_example_db","root","root");
+//        return c;
+//    }   //D사 생성 코드
+//}
